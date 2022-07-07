b7c77ff5216eb2509e62a88869bfbec49f1c8c36
Piotr Makowski-Czerski <piomcz@gmail.com>
2022-07-07 15:49:09 +0200

    Changed docs

--- docs/_build/html/_sources/index.rst.txt
+++ docs/_build/html/_sources/index.rst.txt
@@ -1,5 +1,5 @@
 .. basic-math documentation master file, created by
-   sphinx-quickstart on Thu Jul  7 15:23:11 2022.
+   sphinx-quickstart on Thu Jul  7 15:47:05 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'basic-math'
-copyright = '2022, Piotr Makowski-Czerski'
-author = 'Piotr Makowski-Czerski'
+copyright = '2022, Piotr Makowski'
+author = 'Piotr Makowski'
 
 # The short X.Y version
 version = ''
@@ -44,7 +44,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -65,7 +65,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
@@ -87,7 +87,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -131,7 +131,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'basic-math.tex', 'basic-math Documentation',
-     'Piotr Makowski-Czerski', 'manual'),
+     'Piotr Makowski', 'manual'),
 ]
 
 
